d0ba0ba830b5e0d1d1e84e38cc26d84df9932dd1
Piyush Shewale <92243361+Piyush-OG@users.noreply.github.com>
2024-01-21 16:32:10 +0530

    Create Count Primes.java

---
+++ Day 55/Count Primes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n < 2) {
+            return 0;
+        }
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        int sqrt = (int) Math.sqrt(n);
+        for (int p = 2; p <= sqrt; p++) {
+            if (isPrime[p]) {
+                for (int i = p * p; i < n; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
