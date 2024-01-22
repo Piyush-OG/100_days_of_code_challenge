1472f9fb43b16e024cefea48a08e555404acc117
Piyush Shewale <92243361+Piyush-OG@users.noreply.github.com>
2024-01-22 16:09:30 +0530

    Create Count and Say.java

---
+++ Day 56/Count and Say.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n <= 0) {
+            return "";
+        }
+        String s = "1";
+        for (int i = 2; i <= n; i++) {
+            s = say(s);
+        }
+        return s;
+    }
+
+    private String say(String s) {
+        StringBuffer sb = new StringBuffer();
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            char prev = s.charAt(i - 1);
+            char curr = s.charAt(i);
+            if (curr == prev) {
+                count++;
+            } else {
+                sb.append(count).append(prev);
+                count = 1;
+            }
+        }
+        // append count of last character
+        sb.append(count).append(s.charAt(s.length() - 1));
+
+        return sb.toString();
+    }
+}
