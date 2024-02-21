391f5af0fa1acf50d5b4996ec85f79ca1964ab7b
Piyush Shewale <92243361+Piyush-OG@users.noreply.github.com>
2024-02-21 13:58:35 +0530

    Create Contains Duplicate.java

---
+++ Day 3/Contains Duplicate.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        /*
+         * Time Complexity: O(n) where n = length of nums
+         * Space Complexity: O(n) where n = length of hash set
+         */
+        
+        // If array only has 1 element, return false
+        if (nums.length == 1) {return false;}
+
+        // Create a hash set and iterate array
+        HashSet<Integer> numberSet = new HashSet<Integer>();
+
+        for (int i: nums) {
+
+            // If the number exist in the set, return true
+            if (numberSet.contains(Integer.valueOf(i))) {
+                return true;
+            }
+
+            // If number doesn't exist in set, add it
+            numberSet.add(Integer.valueOf(i));
+        }
+
+        return false;
+    }
+}
